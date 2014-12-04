2f475a2ae7692531ce074c24bdd9b3a2f76aa8b0
John McCrae <john@mccr.ae>
2014-12-04 16:00:39 +0100

    Move backend to Slick to enable different databases

--- scala/src/main/scala/yuzu/sparql.scala
+++ scala/src/main/scala/yuzu/sparql.scala
@@ -6,8 +6,8 @@ import com.hp.hpl.jena.query.{Query, QueryExecution, QueryExecutionFactory, Quer
 import com.hp.hpl.jena.sparql.resultset.ResultSetMem
 import com.hp.hpl.jena.rdf.model.{Model, ModelFactory, AnonId, Resource}
 import com.hp.hpl.jena.util.iterator.ExtendedIterator
-import java.sql.{Connection, PreparedStatement, ResultSet}
 import java.util.concurrent.{Executors, TimeoutException, TimeUnit}
+import scala.slick.driver.SQLiteDriver.simple._
 
 sealed trait SPARQLResult
 
@@ -93,69 +93,25 @@ class SPARQLExecutor(query : Query, qx : QueryExecution) extends Runnable {
 }
 
 
-class RDFBackendGraph(backend : RDFBackend, conn : Connection, stopFlag : StopFlag)  extends com.hp.hpl.jena.graph.impl.GraphBase {
+class RDFBackendGraph(backend : RDFBackend, conn : Session, stopFlag : StopFlag)  extends com.hp.hpl.jena.graph.impl.GraphBase {
   import scala.collection.JavaConversions._
   protected def graphBaseFind(m : TripleMatch) : ExtendedIterator[Triple] = {
     if(stopFlag.isSet) {
-      new NullExtendedIterator()
+      new IterAsExtIter(Nil.iterator)
     } else {
       _graphBaseFind(m) match {
-        case Some((rs,ps)) =>
-          new SQLResultSetAsExtendedIterator(rs,ps, stopFlag)
+        case Some(seq) =>
+          new IterAsExtIter((seq.map {
+            case (s, f, p, o) =>
+              new Triple(RDFBackend.name(s, Option(f)),
+                         prop_from_n3(p),
+                         from_n3(o))
+          }).iterator)
         case None => 
-          new NullExtendedIterator()
+          new IterAsExtIter(Nil.iterator)
       }
     }
   }
-
-  def _graphBaseFind(m : TripleMatch) : Option[(ResultSet,PreparedStatement)] = {
-    val model = ModelFactory.createDefaultModel()
-    val s = m.getMatchSubject()
-    val p = m.getMatchPredicate()
-    val o = m.getMatchObject()
-    if(s != null) {
-      val (id, frag) = RDFBackend.unname(s.toString()) match {
-        case Some((i,f)) => (i,f)
-        case None => 
-          return None
-      }
-      Some(backend.listInternal(conn, Some(id),frag,Option(p).map(RDFBackend.to_n3),Option(o).map(RDFBackend.to_n3)))
-    } else {
-      Some(backend.listInternal(conn, None, None, Option(p).map(RDFBackend.to_n3), Option(o).map(RDFBackend.to_n3)))
-    }
-  }
-}
-
-class NullExtendedIterator() extends ExtendedIterator[Triple] {
-  def close() { }
-  def andThen[X <: Triple](x : java.util.Iterator[X]) : ExtendedIterator[Triple] = throw new UnsupportedOperationException()
-  def filterDrop(x : com.hp.hpl.jena.util.iterator.Filter[com.hp.hpl.jena.graph.Triple]):
-  com.hp.hpl.jena.util.iterator.ExtendedIterator[com.hp.hpl.jena.graph.Triple] = throw new UnsupportedOperationException()
-  def filterKeep(x : com.hp.hpl.jena.util.iterator.Filter[com.hp.hpl.jena.graph.Triple]):
-  com.hp.hpl.jena.util.iterator.ExtendedIterator[com.hp.hpl.jena.graph.Triple] = throw new UnsupportedOperationException()
-  def mapWith[U](x: com.hp.hpl.jena.util.iterator.Map1[com.hp.hpl.jena.graph.Triple,U]): com.hp.hpl.jena.util.iterator.ExtendedIterator[U] =
-    throw new UnsupportedOperationException()
-  def removeNext(): com.hp.hpl.jena.graph.Triple =  throw new UnsupportedOperationException()
-  def toList(): java.util.List[com.hp.hpl.jena.graph.Triple] =  throw new UnsupportedOperationException()
-  def toSet(): java.util.Set[com.hp.hpl.jena.graph.Triple] = throw new UnsupportedOperationException()
-
-  def hasNext(): Boolean = false
-  def next(): Triple = throw new NoSuchElementException()
-  def remove(): Unit = throw new UnsupportedOperationException()
-}
-
-class SQLResultSetAsExtendedIterator(rs : ResultSet, ps : PreparedStatement, stopFlag : StopFlag) extends ExtendedIterator[Triple] {
-  def close() { rs.close(); ps.close() }
-  def andThen[X <: Triple](x : java.util.Iterator[X]) : ExtendedIterator[Triple] = throw new UnsupportedOperationException()
-  def filterDrop(x : com.hp.hpl.jena.util.iterator.Filter[com.hp.hpl.jena.graph.Triple]):
-  com.hp.hpl.jena.util.iterator.ExtendedIterator[com.hp.hpl.jena.graph.Triple] = throw new UnsupportedOperationException()
-  def filterKeep(x : com.hp.hpl.jena.util.iterator.Filter[com.hp.hpl.jena.graph.Triple]):
-  com.hp.hpl.jena.util.iterator.ExtendedIterator[com.hp.hpl.jena.graph.Triple] = throw new UnsupportedOperationException()
-  def mapWith[U](x: com.hp.hpl.jena.util.iterator.Map1[com.hp.hpl.jena.graph.Triple,U]): com.hp.hpl.jena.util.iterator.ExtendedIterator[U] =
-    throw new UnsupportedOperationException()
-  def removeNext(): com.hp.hpl.jena.graph.Triple =  throw new UnsupportedOperationException()
-  def toList(): java.util.List[com.hp.hpl.jena.graph.Triple] =  throw new UnsupportedOperationException()
-  def toSet(): java.util.Set[com.hp.hpl.jena.graph.Triple] = throw new UnsupportedOperationException()
 
   def from_n3(n3 : String) = if(n3.startsWith("<") && n3.endsWith(">")) {
     NodeFactory.createURI(n3.drop(1).dropRight(1))
@@ -182,21 +138,38 @@ class SQLResultSetAsExtendedIterator(rs : ResultSet, ps : PreparedStatement, sto
   }
 
 
-  private var _hasNext = rs.next()
-  def hasNext(): Boolean = {
-    _hasNext && !stopFlag.isSet
+  def _graphBaseFind(m : TripleMatch) : Option[Seq[(String, String, String, String)]] = {
+    val model = ModelFactory.createDefaultModel()
+    val s = m.getMatchSubject()
+    val p = m.getMatchPredicate()
+    val o = m.getMatchObject()
+    if(s != null) {
+      val (id, frag) = RDFBackend.unname(s.toString()) match {
+        case Some((i,f)) => (i,f)
+        case None => 
+          return None
+      }
+      Some(backend.listInternal(conn, Some(id),frag,Option(p).map(RDFBackend.to_n3),Option(o).map(RDFBackend.to_n3)))
+    } else {
+      Some(backend.listInternal(conn, None, None, Option(p).map(RDFBackend.to_n3), Option(o).map(RDFBackend.to_n3)))
+    }
   }
-  def next(): Triple = {
-    val s = rs.getString("subject")
-    val f = rs.getString("fragment")
-    val p = rs.getString("property")
-    val o = rs.getString("object")
-    val t = new Triple(RDFBackend.name(s, Option(f)),
-      prop_from_n3(p),
-      from_n3(o))
-    _hasNext = rs.next()
+}
 
-    return t
-  }
+class IterAsExtIter[A](iter : Iterator[A]) extends ExtendedIterator[A] {
+  def close() { }
+  def andThen[X <: A](x : java.util.Iterator[X]) : ExtendedIterator[A] = throw new UnsupportedOperationException()
+  def filterDrop(x : com.hp.hpl.jena.util.iterator.Filter[A]):
+  com.hp.hpl.jena.util.iterator.ExtendedIterator[A] = throw new UnsupportedOperationException()
+  def filterKeep(x : com.hp.hpl.jena.util.iterator.Filter[A]):
+  com.hp.hpl.jena.util.iterator.ExtendedIterator[A] = throw new UnsupportedOperationException()
+  def mapWith[U](x: com.hp.hpl.jena.util.iterator.Map1[A,U]): com.hp.hpl.jena.util.iterator.ExtendedIterator[U] =
+    throw new UnsupportedOperationException()
+  def removeNext(): A =  throw new UnsupportedOperationException()
+  def toList(): java.util.List[A] =  throw new UnsupportedOperationException()
+  def toSet(): java.util.Set[A] = throw new UnsupportedOperationException()
+
+  def hasNext(): Boolean = iter.hasNext
+  def next(): A = iter.next()
   def remove(): Unit = throw new UnsupportedOperationException()
 }
